a97fc3b43cd6f1aa8b96bfd97fdcf74f6f6dcc00
Dave Stanke <davidstanke@gmail.com>
2021-10-21 14:49:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_cloud_run_service" "whats-the-word" {
         image = "gcr.io/${var.google_project_id}/whats-the-word"
         env {
             name = "APP_HEADER"
-            value = "Where are the spatulas?"
+            value = "What's the word?"
         }
       }
     }
